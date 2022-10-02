7f62188cbba24749fcca826c69429dfcc1f2f37c
sergey_iagfarov <dedaz27@mail.ru>
2022-10-02 16:20:11 +0500

    Домашнее задание

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void task1(String[] args) {
+        int clientOS = 1;
+        if (clientOS == 0) {
+
+            System.out.println("Установите приложение для IOS по ссылке");
+        } else {
+            System.out.println("Установите приложение для Android по ссылке");
+        }
+    }
+}
